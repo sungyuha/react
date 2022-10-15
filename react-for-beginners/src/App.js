d90af31a5c9037cf53ad7242365bd7b049ca148d
YUHASUNG <yuha822@naver.com>
2022-10-16 00:19:22 +0900

    react: Deps

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -2,7 +2,9 @@ import { useState, useEffect } from "react";
 
 function App() {
   const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
   const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
   console.log("i run all the time");
   // useEffect fun 은 코드가 단 한 번만 실행되도록 작동
   useEffect(() => {
@@ -10,9 +12,16 @@ function App() {
   }, []);
   return (
     <div>
+      <input value={keyword} onChange={onChange} type="text" placeholfer="Search here!"/>
       <h1>{counter}</h1>
       <button onClick={onClick}>Click me</button>
-      {/* <Button text={"Continue"} /> */}
+      {/* 
+      1. fun 이 작동할때 argument로 event 받음 
+      2. 그리고 event를 발생시킨 input에서 value를 받아서,
+      3. value를 keyword state에 넣어줌
+      4. 그리고 그 keyword를 가져와서, input의 value로 사용하면
+      5. 원하는 때에 input 작동 가능
+      */}
     </div>
   );
 }
