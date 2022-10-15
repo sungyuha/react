c9c724c1feed7db23ff6338e2e419bbf16138117
YUHASUNG <yuha822@naver.com>
2022-10-16 00:00:12 +0900

    react useEffect 카운더 실습

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -4,10 +4,10 @@ function App() {
   const [counter, setValue] = useState(0);
   const onClick = () => setValue((prev) => prev + 1);
   console.log("i run all the time");
-  const iRunOnlyOnce = () => {
-    console.log("i run only one");
-  };
-  useEffect(iRunOnlyOnce, []);
+  // useEffect fun 은 코드가 단 한 번만 실행되도록 작동
+  useEffect(() => {
+    console.log("CALL THE API...");
+  }, []);
   return (
     <div>
       <h1>{counter}</h1>
