cbfdaf1a7cf391dbd7a6811b2b4751df66074fd9
YUHASUNG <yuha822@naver.com>
2022-10-15 23:53:43 +0900

    react useState, useEffect

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,10 +1,18 @@
-import Button from "./Button";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  console.log("i run all the time");
+  const iRunOnlyOnce = () => {
+    console.log("i run only one");
+  };
+  useEffect(iRunOnlyOnce, []);
   return (
     <div>
-      <h1>Welcome Back!</h1>
-      <Button text={"Continue"} />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>Click me</button>
+      {/* <Button text={"Continue"} /> */}
     </div>
   );
 }
