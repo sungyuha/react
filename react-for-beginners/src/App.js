0d40b4ee5f9297cd3520e9cfc2bb1a872a1b7093
YUHASUNG <yuha822@naver.com>
2022-10-18 15:16:20 +0900

    react useState&useEffect commponent

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -1,14 +1,25 @@
 import { useState, useEffect } from "react";
 
+// 1. 리액트는 component 를 refresh(새로고침)이 가능
+// 2. 새로운 데이터가 들어올때마다 ui를 refresh
+// 3. react.js는 변화가 일어날 때만 refresh함
+// 4. 첫 번째 arrgument는 우리가 실행시키고 싶은 코드
+/* 5. 두 번재는 deoendencies라고 불리우는 것
+deoendencies는 리액트가 지켜보아야 하는것 */
+
 function App() {
   const [counter, setValue] = useState(0);
   const [keyword, setKeyword] = useState("");
+  // keyword는 setKeyword라는 function 으로 변화
   const onClick = () => setValue((prev) => prev + 1);
   const onChange = (event) => setKeyword(event.target.value);
+  // setKeyword는 onChange라는 function에 의해 호출
+
   // console.log("i run all the time");
   // useEffect fun 은 코드가 단 한 번만 실행되도록 작동
   useEffect(() => {
-    console.log("I run only once."); 
+    console.log("I run only once.");
+    // 이 코드는 한 번 만 실행
     // 내가 원하는 콘솔만 찍힘
   }, []);
   // 빈 array를 실행시켰을때 코드가 단 한 번만 실행되는 이유는
@@ -39,6 +50,7 @@ function App() {
   return (
     <div>
       <input value={keyword} onChange={onChange} type="text" placeholder="Search here!"/>
+      {/* onChange는 사용자 input이 변화할 때 호출 */}
       <h1>{counter}</h1>
       <button onClick={onClick}>Click me</button>
       {/* 
@@ -48,6 +60,7 @@ function App() {
       4. 그리고 그 keyword를 가져와서, input의 value로 사용하면
       5. 원하는 때에 input 작동 가능
       */}
+      {/* 리액트는 state를 뱐화시킬 때 component를 재실행시키는 것 */}
     </div>
   );
 }
