d5a252d6c5325315a6860b9265fe969b28adc2eb
YUHASUNG <yuha822@naver.com>
2022-10-18 17:48:29 +0900

    react To do list array에 elemnet 추가

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -2,15 +2,22 @@ import { useState } from "react";
 
 function App(){
   const [toDo, setoDo] = useState("");
+  const [toDos, setoDos] = useState([]); // 수정하는 함수 toDos, setoDos
+  // 기본값으로는 array
   const onChange = (event) => setoDo(event.target.value);
   const onSubmit = (event) => {
     event.preventDefault();
     if ( toDo === "" ) {
       return;
     }
-    setoDo(""); // setoDo는 input을 수정하는 함수
+    /* setoDo(""); // setoDo는 input을 수정하는 함수
     // toDo의 값을 변경하면 input값도 변겯 됨
+    // toDos.push(); -> State를 직접적으로 수정 불가능*/
+  
+    setoDo("");
   };
+    console.log(toDos);
+  // array를 직접적으로 수정하지 않으면서 setToDos로 array에 element를 추가하는 방법
   return (
     <div>
       <form onSubmit={onSubmit}>
