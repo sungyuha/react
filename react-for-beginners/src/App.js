6825aedbdf51d3805f52cb1bc59984ffacae9216
YUHASUNG <yuha822@naver.com>
2022-11-06 22:46:48 +0900

    toDo array

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -13,8 +13,10 @@ function App(){
     /* setoDo(""); // setoDo는 input을 수정하는 함수
     // toDo의 값을 변경하면 input값도 변겯 됨
     // toDos.push(); -> State를 직접적으로 수정 불가능*/
-  
     setoDo("");
+    // array르 직접족으로 수정하지 않으면서 setoDos로 array에 element를 추가하는 방법
+    // 여기에 직전의 값(prevens Value)를 받아올거
+    setoDos(currnetArray => [toDo])
   };
     console.log(toDos);
   // array를 직접적으로 수정하지 않으면서 setToDos로 array에 element를 추가하는 방법
