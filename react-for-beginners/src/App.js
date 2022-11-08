35b6b8207388353dc473b0d70a46cee00dc7530a
YUHASUNG <yuha822@naver.com>
2022-11-08 21:30:49 +0900

    react usestate 상태관리

--- react-for-beginners/src/App.js
+++ react-for-beginners/src/App.js
@@ -13,14 +13,17 @@ function App(){
     /* setoDo(""); // setoDo는 input을 수정하는 함수
     // toDo의 값을 변경하면 input값도 변겯 됨
     // toDos.push(); -> State를 직접적으로 수정 불가능*/
-    setoDo("");
-    // array르 직접족으로 수정하지 않으면서 setoDos로 array에 element를 추가하는 방법
-    // 여기에 직전의 값(prevens Value)를 받아올거
     setoDos(currnetArray => [toDo, ...currnetArray]);
     // 새로운 array는 state에 있는 toDo
     // 더 정확하게 하기 위해서 currnetArray라고 명시 / 그리고 뒤에 함수
+    // 수정하는 함수를 사용할 때 두 가지 옵션이 있음
+    // 함수를 보낼 때 react.js는 함수의 첫 번째 argument를 현재 state로 보냄
+    setoDo("");
+    // 우리가 지정한 data를 가지고 있음
+    // 여기에 직전의 값(prevens Value)를 받아올거. 
+    // array르 직접족으로 수정하지 않으면서 setoDos로 array에 element를 추가하는 방법
   };
-    console.log(toDos);
+  console.log(toDos);
   // array를 직접적으로 수정하지 않으면서 setToDos로 array에 element를 추가하는 방법
   return (
     <div>
