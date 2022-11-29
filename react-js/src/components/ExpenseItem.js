2928ea7262eaa83b813e763e5f5ef6f52836c121
YUHASUNG <yuha822@naver.com>
2022-11-30 01:11:41 +0900

    props를 통해서 데이터 전달

--- react-js/src/components/ExpenseItem.js
+++ react-js/src/components/ExpenseItem.js
@@ -7,12 +7,12 @@ function ExpenseItem() {
 
     return (
         <div className="expense-item">
-            <div>{expenseDate}</div>
+            <div>{expenseDate.toISOString()}</div>
             <div className="expense-item__description">
                 {/* <h2>{Math.random()}</h2> */}
                 {/* 내장 된 math객체로 임의의 숫자를 생성함 */}
                 <h2>{expenseTitle}</h2>
-                <div className="expense-item__price">{expenseAmount}</div>
+                <div className="expense-item__price">${expenseAmount}</div>
             </div>
         </div>
     )
