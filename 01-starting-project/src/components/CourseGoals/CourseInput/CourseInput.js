c8db881b287d90683019fb64b9f2f7940e41196b
YUHASUNG <yuha822@naver.com>
2022-12-04 00:03:11 +0900

    동적 인라인 스타일 설정

--- 01-starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
+++ 01-starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -5,6 +5,8 @@ import './CourseInput.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [isValid, setIsvalid] = useState(true);
+  // 값이 true이면, 입력값이 유효하고 
 
   const goalInputChangeHandler = event => {
     setEnteredValue(event.target.value);
@@ -13,7 +15,10 @@ const CourseInput = props => {
   const formSubmitHandler = event => {
     event.preventDefault();
     // trim는 시작이나 끝 부분에 과도하게 쓰인 공백을 제거해주는 매소드
-    if (enteredValue.trim())
+    // 유효하지 않으면 setIsvalid를 호출
+    if (enteredValue.trim().length === 0) {
+      return;
+    }
     props.onAddGoal(enteredValue);
   };
 
