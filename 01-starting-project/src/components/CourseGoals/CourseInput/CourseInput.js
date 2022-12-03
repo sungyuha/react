e3d68679cebc4d5bbc49e1b4a6b9a2b02bcdc9c9
YUHASUNG <yuha822@naver.com>
2022-12-03 22:06:13 +0900

    리액트 컴포넌트 스타일링

---
+++ 01-starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+
+import Button from '../../UI/Button/Button';
+import './CourseInput.css';
+
+const CourseInput = props => {
+  const [enteredValue, setEnteredValue] = useState('');
+
+  const goalInputChangeHandler = event => {
+    setEnteredValue(event.target.value);
+  };
+
+  const formSubmitHandler = event => {
+    event.preventDefault();
+    // trim는 시작이나 끝 부분에 과도하게 쓰인 공백을 제거해주는 매소드
+    if (enteredValue.trim())
+    props.onAddGoal(enteredValue);
+  };
+
+  return (
+    <form onSubmit={formSubmitHandler}>
+      <div className="form-control">
+        <label>Course Goal</label>
+        <input type="text" onChange={goalInputChangeHandler} />
+      </div>
+      <Button type="submit">Add Goal</Button>
+    </form>
+  );
+};
+
+export default CourseInput;
