7b8d416ddf2a03a198e79465cf978ff3ef843175
sungyuha <yuha822@naver.com>
2022-12-04 21:36:08 +0900

    동적으로 인라인 스타일 설정

--- 01-starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
+++ 01-starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -9,6 +9,9 @@ const CourseInput = props => {
   // 값이 true이면, 입력값이 유효하고 
 
   const goalInputChangeHandler = event => {
+    if (event.target.value.trim().length > 0) {
+      setIsvalid(true);
+    }
     setEnteredValue(event.target.value);
   };
 
@@ -17,6 +20,7 @@ const CourseInput = props => {
     // trim는 시작이나 끝 부분에 과도하게 쓰인 공백을 제거해주는 매소드
     // 유효하지 않으면 setIsvalid를 호출
     if (enteredValue.trim().length === 0) {
+      setIsvalid(false);
       return;
     }
     props.onAddGoal(enteredValue);
@@ -25,8 +29,12 @@ const CourseInput = props => {
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label>Course Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <label style={{color: !isValid ? 'red' : 'black'}}>Course Goal</label>
+        <input style={{
+          borderColor: !isValid ? 'red' : '#ccc',
+          backhround: !isValid ? 'salmon' : 'transparent'
+          }} 
+          type="text" onChange={goalInputChangeHandler} />
       </div>
       <Button type="submit">Add Goal</Button>
     </form>
