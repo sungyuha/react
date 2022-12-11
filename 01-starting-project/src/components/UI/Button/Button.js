4c5efafee7ad4eef98e0bf724c010d0d2c2df98d
YUHASUNG <yuha822@naver.com>
2022-12-11 21:44:28 +0900

    styled-components

--- 01-starting-project/src/components/UI/Button/Button.js
+++ 01-starting-project/src/components/UI/Button/Button.js
@@ -1,13 +1,39 @@
 import React from 'react';
+// styled는 styled-components에서 임포트 하는 객체 -> Button메소드에 접근 가능
+import styled from 'styled-components';
 
 import './Button.css';
 
-const Button = props => {
+// // styled-components 별로 고유 -> 스타일이 지정 됨
+const Button = styled.Button`
+  .button {
+    font: inherit;
+    padding: 0.5rem 1.5rem;
+    border: 1px solid #8b005d;
+    color: white;
+    background: #8b005d;
+    box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+    cursor: pointer;
+  }
+  
+  .button:focus {
+    outline: none;
+  }
+  
+  .button:hover,
+  .button:active {
+    background: #ac0e77;
+    border-color: #ac0e77;
+    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
+  }
+`; // button 은 stylde 객체의 메소드
+
+/*const Button = props => {
   return (
     <button type={props.type} className="button" onClick={props.onClick}>
       {props.children}
     </button>
   );
-};
+};*/
 
 export default Button;
