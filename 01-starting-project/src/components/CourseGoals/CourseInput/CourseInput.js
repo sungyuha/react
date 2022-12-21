2c431aeb9321f93b9fbe9e9232d2d2738a5ace2f
YUHASUNG <yuha822@naver.com>
2022-12-21 22:03:36 +0900

    styled components

--- 01-starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
+++ 01-starting-project/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,7 +1,32 @@
 import React, { useState } from 'react';
-
+import styled from 'styled-components';
 import Button from '../../UI/Button/Button';
 import './CourseInput.css';
+
+const FormControl = styled.div`
+    margin: 0.5rem 0;
+
+  & label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+  }
+
+  & input {
+    display: block;
+    width: 100%;
+    border: 1px solid #ccc;
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+  }
+
+  & input:focus {
+    outline: none;
+    background: #fad0ec;
+    border-color: #8b005d;
+  }
+`;
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
@@ -28,14 +53,16 @@ const CourseInput = props => {
 
   return (
     <form onSubmit={formSubmitHandler}>
-      <div className={`form-control ${!isValid ? 'invalid' : ''}`}>
+      <FormControl>
+      {/* <div className={`form-control ${!isValid ? 'invalid' : ''}`}> */}
         <label style={{color: !isValid ? 'red' : 'black'}}>Course Goal</label>
         <input style={{
           borderColor: !isValid ? 'red' : '#ccc',
           backhround: !isValid ? 'salmon' : 'transparent'
           }} 
           type="text" onChange={goalInputChangeHandler} />
-      </div>
+      {/* </div> */}
+      </FormControl>
       <Button type="submit">Add Goal</Button>
     </form>
   );
--- 01-starting-project/src/components/UI/Button/Button.js
+++ 01-starting-project/src/components/UI/Button/Button.js
@@ -4,36 +4,34 @@ import styled from 'styled-components';
 
 import './Button.css';
 
-// // styled-components 별로 고유 -> 스타일이 지정 됨
-/*const Button = styled.Button`
-  .button {
-    font: inherit;
-    padding: 0.5rem 1.5rem;
-    border: 1px solid #8b005d;
-    color: white;
-    background: #8b005d;
-    box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
-    cursor: pointer;
-  }
-  
-  .button:focus {
+// styled-components 별로 고유 -> 스타일이 지정 됨
+const Button = styled.button`
+  font: inherit;
+  padding: 0.5rem 1.5rem;
+  border: 1px solid #8b005d;
+  color: white;
+  background: #8b005d;
+  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+  cursor: pointer;
+
+  &:focus {
     outline: none;
   }
   
-  .button:hover,
-  .button:active {
+  &:hover,
+  &:active {
     background: #ac0e77;
     border-color: #ac0e77;
     box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
   }
 `; // button 은 stylde 객체의 메소드*/
 
-const Button = props => {
+/*const Button = props => {
   return (
     <button type={props.type} className="button" onClick={props.onClick}>
       {props.children}
     </button>
   );
-};
+};*/
 
 export default Button;
