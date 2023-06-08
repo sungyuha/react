d446cb2e51582586a314808ed1f5e5a18dcce7b4
YUHASUNG <yuha822@naver.com>
2023-06-08 15:51:27 +0900

    dosc: React Hooks - useCallback

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -4,10 +4,11 @@ function App() {
     const [number, setNumber] = useState(0);
 
 
-    const someFunction = useCallback( () => { // someFunction 안에 메모리의 주소를 가지고 있음
-        console.log(`someFunc: number: ${number}`); // App 컴포넌트가 랜더링 될때마다 초기화가 된다면
+    const someFunction = useCallback(() => { // someFunction 안에 함수 겍체가 메모이제이션
+        console.log(`someFunc: number: ${number}`); 
         return; 
-    },[]); // 의존성 배열에 아무것도 없으니 App 컴포넌트가 만들아질 때미디 someFunction 객체가 메모이제이션 됨
+        // 의존성 배열에 아무것도 없으니 App 컴포넌트가 만들아질 때미디 someFunction 객체가 메모이제이션 됨
+    },[number]);// number state가 업데이트 될때마다 메모이제이션 된 함수도 업데이트 시켜주고 싶으면 두 번째 인자에 들어있는 의존성 배열에 number
 
     useEffect(() => {
         console.log("someFunction이 변경되었습니다.");
