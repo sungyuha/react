a3b6c2ab57aa5bbc1d6de6907d8772b7924e4818
YUHASUNG <yuha822@naver.com>
2023-06-09 11:07:12 +0900

    dosc: React - useCallback

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,24 +1,33 @@
-import React, {useState} from "react";
+import React, {useCallback, useState} from "react";
 import Box from './Box';
 
 function App() {
     const [size, setSize] = useState(100);
+    const [isDark, setIsDark] = useState(false);
 
-    const createBoxStyle = () => {
+    const createBoxStyle = useCallback( () => {
+        // useCallback의 첫 번째 인자로 함수를 넣어줌
         return {
             backgroundColor: 'pink',
             width: `${size}px`,
             height: `${size}px`,
         };
-    };
+        // 의존성 배열에는 size. size가 변경될때마다 초기화가 됐으면 좋겠으니끼
+    }, [size]);
 
     return (
-        <div>
+        <div style={{
+            // 만약에 isDark가 ture라면 상위 div의 색상을 블랙으로, isDark가 false라면 white로
+            background: isDark ? "black": "white",
+            }}
+        >
             <input 
                 type="number"
                 value={size}
                 onChange={(e) => setSize(e.target.value)}
             />
+            <button onClick={() => setIsDark(!isDark)}>테마 체인지</button>
+            {/* 뤂업으로 전달 받음 */}
             <Box createBoxStyle={createBoxStyle} />
         </div>
     );
--- react-app/src/Box.js
+++ react-app/src/Box.js
@@ -1,10 +1,12 @@
 import React, {useEffect, useState} from "react";
 
 const Box = ( {createBoxStyle} ) => {
+    // 맨 처음은 빈 오브젝트로 초기화를 시켜줌
     const [style, setStyle] = useState({});
 
     useEffect(() => {
-        console.log('박스 키우기');
+        console.log('박스 키우기 🎨');
+        // useEffect는 createBoxStyle가 리턴한 값을 setStyle로 
         setStyle(createBoxStyle());
     }, [createBoxStyle]);
 
