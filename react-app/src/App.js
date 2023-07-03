177916625bb29529c7738d3b6f2540091fc94090
YUHASUNG <yuha822@naver.com>
2023-07-03 11:51:53 +0900

    dosc: React.Fragment

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,26 +1,13 @@
-import { useFetch } from "./useFetch";
-
-// 변수 baseUrl
-const baseUrl = "https://jsonplaceholder.typicode.com";
+import React from "react";
+import Component from "./Component";
 
 function App() {
-    // fetchUrl 전달 받음
-    const {data, fetchUrl} = useFetch(baseUrl, 'users');
-
     return (
-        <div>
-            {/* fetch api를 통해서 네트워크 상에서 데이터를 가져오는 기능을 해주는 커스텀훅 */}
-            <h1>useFetch</h1>
-            {/* Users버튼이 클릭하면 만든 fetchUrl 함수 호출. 대신 uesr에 관한 데이터 받아옴 */}
-            <button onClick={() => fetchUrl('users')}>Users</button>
-            <button onClick={() => fetchUrl('posts')}>Posts</button>
-            <button onClick={() => fetchUrl('todos')}>Todos</button>
-            {/* pre 태그를 사용해서 출력. 데이터는 json 형태 */}
-            {/* 첫 번째는 data, 두 번째 인자로는 Null, 세 번째 인자로는 2 */}
-            {/* 받아와진 정보는 pre 태그를 통해 화면에 출력 */}
-            <pre>{JSON.stringify(data, null, 2)}</pre>
-        </div>
+        <>
+            {/* <h1>React.Fragment</h1> */}
+            <Component />
+        </>
     );
-}
+};
 
 export default App;
---
+++ react-app/src/Component.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Component = () => {
+    return (
+        // React.Fragment 생략해서 작성 가능
+        <>
+            <h1>안녕하세요 리액트</h1>
+            <p>반가워요</p>
+        </>
+    );
+};
+
+export default Component;
