ab68eee5599d725815162c5de804d2d671069271
YUHASUNG <yuha822@naver.com>
2023-07-05 09:31:59 +0900

    dosc: React - ForwardRef 사용시 주의점

--- react-app/src/ForwardRef.js
+++ react-app/src/ForwardRef.js
@@ -27,3 +27,18 @@ function ForwardRef() {
 };
 
 export default ForwardRef;
+/* 
+React의 ForwardRef란? | Ref 전달하기
+<Parent>
+const myRef = useRef()
+            ↘
+<Child ref={useRef}>
+1) 부모 컴포넌트에서 자녀 컴포넌트에게 ref를 전달할 때 ForwardRef를 사용하면 
+마치 기본적인 엘리먼트에다가 ref를 전달하듯 전달해줄 수 있으니까 정말 편리함!!
+2) 특히나 자주 사용되는 재사용성이 높은 자녀 컴포넌트라면 ForwardRef를 사용해서 더 유연하게 돔 요소에 접근 가능
+3) 
+(1) 지나친 유용성은 단점을 불러옴!! ForwardRef를 사용해서 자녀의 돔 요소에 접근한다는 것은
+(2) 자녀 컴포넌트의 캡슐화에 대한 장점을 없애버리는 의미(왜냐면 자녀 컴포넌트가 가진 돔 노드를 외부로 노출시키는거라서)
+(3) 그렇기 때문에 필요하지 않으면 ForwardRef를 사용하지 않는 편이 좋음!
+4) 그렇지만 엘리먼트에 포커스를 준다던지 -> focus(), 애니메이션을 관리 해야한다던지, 돔 노드에 접근하는게 불가피할때는 ForwardRef를 사용하면 됨
+*/
