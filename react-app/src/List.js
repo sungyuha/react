a09bf692d131a5ce3641a6c202f69d40d47b61d2
YUHASUNG <yuha822@naver.com>
2023-07-06 21:39:49 +0900

    dosc: React - List와 Key의 중요성

--- react-app/src/List.js
+++ react-app/src/List.js
@@ -1,5 +1,3 @@
-// 리스트 형태로 만든 엘리먼트들은 꼭 안정적이고 고유한 Key값을 가져야 함
-
 import { useState } from "react";
 
 function List() {
@@ -29,7 +27,15 @@ function List() {
             <ul>
                 {/* list에 map이라는 함수 사용해서 배열 안에 있는 항복들을 돌면서 li태그를 사용해서 화면에 출력 */}
                 {list.map((item) => {
+                    /*
+                    1) 현재는 list 안에 key 값을 넣지 않아서 오류 발생
+                    2) 돔 요소에서 어떤 항복이 업데이트 됐는지 알 수 없어서 모든 항목들을 전부 업데이트 함ㄴ
                     return <li>{item}</li>
+                    */
+                    
+                    // 각 항목에 key값을 넣어줌
+                    // 리스트 형태로 만든 엘리먼트들은 꼭 안정적이고 고유한 Key값을 가져야 함(중요!)
+                    return <li key={item}>{item}</li>
                 })}
             </ul>
         </>
