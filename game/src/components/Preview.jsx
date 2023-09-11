121e5dc61c36d7cc917a34742cd504d2df021d5a
YUHASUNG <yuha822@naver.com>
2023-09-11 19:28:43 +0900

    dosc: React Tetris - Preview 기능 및 화면 구현

--- game/src/components/Preview.jsx
+++ game/src/components/Preview.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
+import builBoard from '../business/Board';
+import BoardCell from './BoardCell';
+import transferToBoard from '../business/transferToBoard';
 
-const Preview = ({tetromino}) => {
+const Preview = ({tetromino, idx}) => { 
+    // 모양이 있어야하고, 클래스 이름이 있어야 함
+    const {shape, className} = tetromino;
+
+    // 4행 4열의 보드를 만들고 테트리스 조각 미리보기에 넣어진 조그마한 보드
+    const board = builBoard({rows: 4, columns: 4});
+
+    // 랜더링 되는 미리보기에 따라 상단에 배치 -> 겹치지 않게 하기 위해 스타일은 동적으로 표시
+    const style = {top: `${idx * 15}vw`};
+
+    // 미리보기를 사용하고 있는 테트리스 조각을 보드로 아래 하단과 같이 전송
+    board.rows = transferToBoard({
+        className,
+        isOccupied: false,
+        position: { row: 0, column: 0 },
+        rows: board.rows,
+        shape
+    });
+
     return (
-        <>
-            
-        </>
+        <div className='piece_preview' style={style}>
+            <div className='preview-board'>
+                <BoardCell />
+            </div>
+        </div>
     );
 }
 
-export default Preview;
+// React.memo를 사용하여 구성요소를 반환
+export default React.memo(Preview);
--- game/src/components/Previews.jsx
+++ game/src/components/Previews.jsx
@@ -13,7 +13,11 @@ const Previews = ({tetrominoes}) => { // tetrominoes를 가져옴
             {/* 데이터가 변경 될 때 데이터 랜더링 */}
             {/* 각 개별 테트리스 조각에 대해 개별 미리보기를 모두 반환 */}
             {PreviewTetrominoes.map((tetromino, idx) => (
-                /** 주어진 idx의 각 테트리스 조각에 대해 해당 테트리스의 미리보기를 idx값으로 랜더링  */
+                /** 
+                 * 주어진 idx의 각 테트리스 조각에 대해 해당 테트리스의 미리보기를 idx값으로 랜더링  
+                 * 같은 항목에 매핑할 때마다 키를 갖게 됨
+                 * key의 고유항목을 넣어야 하는데 현재는 idx값으로 작성
+                */
                 <Preview tetromino={tetromino} index={idx} key={idx} />
             ))}
         </>
