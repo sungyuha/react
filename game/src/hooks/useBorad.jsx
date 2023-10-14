250e17045fd29e987bfa3cc98b2a3f5fef7e893f
YUHASUNG <yuha822@naver.com>
2023-10-14 09:17:04 +0900

    dosc: Tetris - board 업데이트 시

--- game/src/hooks/useBorad.jsx
+++ game/src/hooks/useBorad.jsx
@@ -1,5 +1,5 @@
-import { useState } from 'react'
-import buildBoard from '../business/buildBoard';
+import { useState, useEffect } from 'react'
+import { buildBoard, nextBoard } from '../business/buildBoard';
 
 export const useBorad = ({
         rows,
@@ -10,7 +10,19 @@ export const useBorad = ({
     }) => {
     // 초기 보드 상태
     const [board] = useState(buildBoard({rows, columns})); // 함수를 호출하여 행과 열로 보드 제작
-
+    
+    useEffect(() => {
+        // 보드를 업데이트
+        setBoard((previousBoard) =>
+            nextBoard({
+                board: previousBoard,
+                player,
+                resetPlayer,
+                addLinesCleared
+            })
+        );
+        // 상태 중 하나가 플레이어를 재설정하거나 라인이 지워질때마다 보드를 업데이트
+    }, [player, resetPlayer, addLinesCleared]); // 이 값들은 변경 되어선 안됨
     // 초기 보드 설정의 종류를 반환
     return [board];
 }
