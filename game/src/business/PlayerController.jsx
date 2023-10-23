c05b585f1f26e4d2c97a03b6477276363bc5f063
YUHASUNG <yuha822@naver.com>
2023-10-23 15:15:09 +0900

    dosc: Tetris - player position

--- game/src/business/PlayerController.jsx
+++ game/src/business/PlayerController.jsx
@@ -1,3 +1,4 @@
+import { hasCollision, isWithinBoard } from "./business/Board";
 import { rotate } from './business/Tetrominoes';
 
 // 플레이 컨트롤러
@@ -6,4 +7,22 @@ const attemptRotation = ({ board, player, setPlayer }) => {
         piece: player.tetromino.shape,
         direction: 1
     });
-}    
+
+// 플레이어 포지션
+const position = player.position;
+    const isValidRotation =
+        isWithinBoard({ board, position, shape }) &&
+        !hasCollision({ board, position, shape });
+
+    if (isValidRotation) {
+        setPlayer({
+        ...player,
+        tetromino: {
+            ...player.tetromino,
+            shape
+        }
+        });
+    } else {
+        return false;
+    }
+};
