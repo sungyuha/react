572e4990dc2dcd922ad39f41a0286f6961cd24e0
YUHASUNG <yuha822@naver.com>
2023-11-01 19:30:58 +0900

    dosc: Chat - Schema 데이터 구조 설정

--- chat/server/Model/uesr.js
+++ chat/server/Model/uesr.js
@@ -2,3 +2,15 @@
 
 const mongoose = require('mongoose');
 
+// 내가 받을 데이터를 설명
+const userSchema = new mongoose.Schema({
+    name: { // 유저 이름
+        type: String,
+        require: [true, "User must type name"], // 사용자는 이름을 기억해애 해!
+        nuique: true,
+    },
+    token: { // 연결 id 정보
+        type: String
+    }
+});
+module.exports = mongoose.module("User", userSchema);
