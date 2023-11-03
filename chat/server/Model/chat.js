92bc1ee006bb1ea826d6a1ee380d1867adeeff41
YUHASUNG <yuha822@naver.com>
2023-11-03 20:52:41 +0900

    dosc: Chat - DB chat 정보 셋팅

--- chat/server/Model/chat.js
+++ chat/server/Model/chat.js
@@ -1,14 +1,17 @@
 const mongoose = require('mongoose');
 
 const chatSchema = new mongoose.Schema(
-    {
-        chat: String,
-        user: {
-            id: {
+    { // 메세지 정보를 받아옴
+        chat: String, // 메세지
+        user: { // 메세지 보낸 유저 정보를 객체로 저장
+            id: { // 유저의 아이디 값
+                type: mongoose.Schema.ObjectId,
                 ref: "User",
-            }
+            },
+            name: String, // 유저의 이름 값
         },
     },
+    {timestamp:true}
 );
 
-module.exports = mongoose.module("User", chatSchema);
+module.exports = mongoose.module("Chat", chatSchema);
