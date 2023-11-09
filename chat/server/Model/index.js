2bcfd40f5a8cb462773ca1b0a60f76c9523af3f5
YUHASUNG <yuha822@naver.com>
2023-11-09 10:20:11 +0900

    dosc: Chat - project 기본 셋팅

--- chat/server/Model/index.js
+++ chat/server/Model/index.js
@@ -3,7 +3,7 @@ const {createServer} = require("http");
 const app = require("./app");
 // 웹소켓
 const {Server} = require("socket.io");
-
+require("dotenv").config();
 // DB연결
 const httpServer = createServer(app);
 // 웹소켓 -> io는 통상적으로 자주 사용하는 웹소켓 약어
@@ -11,5 +11,10 @@ const io = new Server(httpServer, {
     cors: {
         // 클라이언트 주소===프론트엔드
         origin: "http://localhost:3000"
-    }
+    },
+});
+
+// 포트 넘버 오픈
+httpServer.listen(process.env.PORT, () => {
+    console.log("Server listening on port", process.env.PORT);
 });
