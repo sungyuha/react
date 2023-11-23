80116eea9a5eca83d7dc3d343aea9866145bb371
YUHASUNG <yuha822@naver.com>
2023-11-23 20:33:24 +0900

    dosc: Chat - 간단 socket 입력창에 이름을 입력하면 유저 로그인

--- chat/client/src/App.js
+++ chat/client/src/App.js
@@ -1,17 +1,35 @@
+import { useEffect } from 'react';
+import './App.css';
+import socket from './server';
+
 function App() {
+  useEffect(() => {
+    askUserName();
+  }, []);
+
+  const askUserName = () => {
+    const userName = prompt('당신의 이름을 입력하세요'); // 웹 사이트에 이름을 입력해서 로그인하면 프롬프트 호출
+    console.log('uuu', userName);
+
+    socket.emit('login', userName, (res) => { // emit(대화의 제목, 보낼내용, 콜백함수)
+      console.log('Res', res);
+    }) // emit함수가 잘 처리가되면 마지막에 콜백함수로 응답 받음
+  };
+
   return (
     <div>
-      <h2>Hello chat</h2>
-      {/* 
-        1. 백엔드: 데이터베이스 셋팅, 웹소켓 셋팅
-          - 저장하고 싶은 데이터는 딱 2가지! -> 1) 유저 정보, 2) 채팅 메세지 정보
-        2. 프론트엔드 : 웹소켓 셋팅
-        3. 백엔드, 프론트엔드 연결 테스트
-        4. 유저 로그인
-        5. 메세지 주고받기(서로 다른 브라우저로 접속해서)
-      */}
+      
     </div>
   );
 }
+
+/* 
+  1. 백엔드: 데이터베이스 셋팅, 웹소켓 셋팅
+    - 저장하고 싶은 데이터는 딱 2가지! -> 1) 유저 정보, 2) 채팅 메세지 정보
+  2. 프론트엔드 : 웹소켓 셋팅
+  3. 백엔드, 프론트엔드 연결 테스트
+  4. 유저 로그인
+  5. 메세지 주고받기(서로 다른 브라우저로 접속해서)
+*/
 
 export default App;
