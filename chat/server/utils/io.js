549e4db6c9062e3a43ec95eab644b841e262a1bd
YUHASUNG <yuha822@naver.com>
2023-11-25 20:21:18 +0900

    dosc: Chat - 유저 정보 DB에 저장 & 에러핸들링

--- chat/server/utils/io.js
+++ chat/server/utils/io.js
@@ -1,10 +1,19 @@
+import userContorller from '../Contorllers/user_contorllers';
+
 // 통신 관련 된 함수
 module.exports = function(io) { // io를 매개변수로 받아옴
     io.on("connection", async(socket) => {
         console.log("client is connected", socket.id);
 
-        socket.on('login', async(userName, callback) => { // login 연결
-             // 받은 유저 정보를 저장하고 소켓 아이디 저장
+        socket.on('login', async(userName, cb) => { // login 연결
+            // 에러 핸들링
+            try { // 괜찮은 케이스
+                // 받은 유저 정보를 저장하고 소켓 아이디 저장
+                const user = userContorller.saveUser(userName, socket.id);
+                cb({ok:true, deta:user}) // cb: 콜백함수
+            } catch(error) { // 에러 케이스
+                cb({ok: false, error:error.massage}) // 콜백함수 재호출 -> 에러메시지 출력
+            }
         });
         
 
